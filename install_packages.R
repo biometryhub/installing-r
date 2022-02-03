409029aa42970372c6417b27172bcff3e3708a63
Sam Rogers <7007561+rogerssam@users.noreply.github.com>
2022-02-03 11:24:32 +1030

    Adding package script

---
+++ install_packages.R
@@ -0,0 +1,46 @@
+required_packages <- function() {
+    required <- c(
+        # tidyverse
+        "tidyverse",
+        # Rstudio
+        "rmarkdown",
+        # W3 packages
+        "tinytex", "pander", "kableExtra", "remotes",
+        # biometryassist
+        "biometryassist"
+    )
+
+    if(Sys.info()[['sysname']]!="Linux") {
+        oo <- options(install.packages.compile.from.source = "never")
+        on.exit(options(oo))
+    }
+
+    installed <- rownames(installed.packages())
+    notInstalled <- setdiff(required, installed)
+
+    if (length(notInstalled) > 0){
+
+        message("Installing required packages...\n")
+        install.packages(notInstalled, quiet = TRUE,
+                         Ncpus = getOption("Ncpus", max(parallel::detectCores() - 2, 1)))
+
+        installed <- rownames(installed.packages()) # Update to see what's there now
+        missingPackages <- setdiff(required, installed)
+
+
+        if (length(missingPackages) > 0){
+            invisible(sapply(missingPackages,
+                             function(x){
+                                 warning("\nThe package ", x, " has not installed sucessfully.\n",
+                                         "Please copy and paste the code:\n\n\t install.packages('", x,
+                                         "')\n\n and try running it again on the console. \n\n
+                                 If that fails again, please email the output to biometrytraining@adelaide.edu.au\n")
+                             }))
+            stop("Installation unsuccessful.")
+        }
+        else{
+            message("\nAll required packages have been installed.\n\n\n")
+        }
+    }
+
+}
